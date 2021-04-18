7f72e7fd660ceb01456751a000c5dcd00e33d4b2
rahulmohonto <rahulmohonto1999@gmail.com>
2021-04-18 20:02:20 +0600

    blog updated

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 ## Aditional featurs
 
-added login button with carousel items which redirects to login page and alsoi has social link to connect and more details about aout company.
+Added login button with carousel items which redirects to login page and alsoi has social link to connect and more details about aout company.
 
 ## Hosted in firebase and server is hosted in heroku.
+
+you can add admin and add services if you are admin.
---
+++ src/components/Home/Blogs/Blogs.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div className="text-justify bg-brand">
+            <div className="">
+                <h3 style={{ color: 'dodgerblue' }}>Bus Rent</h3>
+                <span>
+                    <h4 style={{ color: 'pink' }}>Check out the bus rental provider</h4>
+                    <p style={{ color: 'hotpink' }}>
+                        It is always advisable to check reviews and referrals to hiring the best company. Your bus rental service should not be a random choice, so it is always important to do detailed research to select a good bus rental service.</p>
+                    <h4 style={{ color: 'dodgerblue' }}>
+                        Make reservations</h4>
+                    <p style={{ color: 'hotpink' }}>
+                        With the onset of the festive season, you might want to plan things in advance. To ensure your trip does no get hampered, you may want to reserve at two months in advance and go ahead to confirm the reservations a week before the D-day. Usually, weddings and other special occasions are held during weekends or holidays, and this is the time reservations are a bit tricky to make because everyone else is looking to make their event a success.</p>
+                </span>
+            </div>
+            <div className="training">
+                <h3 style={{ color: 'dodgerblue' }}>Driving Training</h3>
+                <p style={{ color: 'hotpink' }}>
+                    This two-week training program ensures you are fully prepared with the knowledge and skills to safely operate a commercial bus or school bus and you are able to obtain a Class B Commercial Driver’s License (CDL) with endorsements. You will receive 80 hours of instruction in a classroom, on driving simulators, and by driving a full-length bus on the driving range and on the road. Topics covered will include basic operations of the bus, safe operating practices, advanced operating practices, vehicle maintenance, and non-vehicle maintenance
+                </p>
+                <h6 style={{ color: 'pink' }}>  Career Opportunities </h6>
+                <p style={{ color: 'hotpink' }}>
+
+                    Upon successful completion, you will have the knowledge and practical skills necessary to qualify for entry-level bus driver positions with school districts, private and local government transit systems, city motor coach companies, tour bus companies, casino transportation, and limousine transportation services.
+                    </p>
+
+            </div>
+            <div>
+                <h3 style={{ color: 'dodgerblue' }}>Bus Repair</h3>
+                <p style={{ color: 'hotpink' }}>
+                    Atlantic Bus offers Bus Servicing and Bus Repairs in our state of the art service and repair facility located in South Florida. Whether you have one bus or an entire fleet of buses or vehicles we have been providing quality services at competitive prices for over 30 years!
+
+                    Our bus repair department is committed to customer service. We pride ourselves in providing courteous, professional, expedited service. At Atlantic bus Sales our service and repair customers are a top priority. When your bus is down you can’t afford to wait, we want t o get you in and out as quickly as possible without sacrificing the quality of your repairs.
+                </p>
+            </div>
+            <div>
+                <h3 style={{ color: 'dodgerblue' }}> Engine Replacement</h3>
+                <p style={{ color: 'hotpink' }}>
+                    On the plus side you have a very good engine. The 345 seems to be the holy grail of the IHC engines for guys running Scouts and the trails and I can't disagree with their reputation as being stout performers. Every one I've ever seen has had a forged crank, forged rods, etc. They really are built well, but as you learned....they have a relatively low redline. My suggestion would be to rebuild what you have. Chances are you will only need to bore it (or even just hone it if you're lucky) and put some new gaskets, pistons, and rings in it. Though it's not cheap, it is easily your cheapest option short of running a used motor you pull out of some other vehicle.
+
+                    The two speed rear end is of course a good option though might be expensive. Another option might be finding an aux. tranny. A final option if you really want to get frisky might be to take a divorced type rockwell transfer case and run it backwards so that low range is now a huge overdrive. I know it sounds kind of booty fab, but I've seen worse done for cheaper with god results
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Blogs;
